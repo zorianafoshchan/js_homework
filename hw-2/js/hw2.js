65bd1a9ac77a729adb180b5699b6ef59b0844f87
zorianafoshchan <zoryana0403@gmail.com>
2023-05-03 17:19:34 +0300

    hw2 almost complete

---
+++ hw-2/js/hw2.js
@@ -0,0 +1,42 @@
+// 3
+let word = prompt('введіть слово')
+
+switch (word) {
+ case 'арматура':
+  alert('armature, steel reiforcement')
+  break;
+ case `в'язниця`:
+  alert('jail, prison')
+  break;
+ case 'галактика':
+  alert('galaxy')
+  break;
+ case 'заочник':
+  alert('extramural student')
+  break;
+ case 'мідія':
+  alert('mussel')
+  break;
+ case 'ледар':
+  alert('idler, loafer, slacker, dawdler')
+  break;
+ case 'неможливість':
+  alert('impossibility, inability')
+  break;
+ case 'ондатра':
+  alert('muskrat')
+  break;
+ case 'шлафрок':
+  alert('dressing gown')
+  break;
+ case 'креденець':
+  alert('sideboard, buffet, cupboard')
+  break;
+
+ default:
+  alert('Not yet in this dictionary!')
+}
+
+// 4
+let books = ['book1', 'book2', 'book3', 'book4', 'book5']
+console.log(books);
